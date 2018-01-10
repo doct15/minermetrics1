e3e10860356f9ac0883b8578a06c33d7ce5325a3
Brian McGehee <doc@tavian.com>
2018-01-09 20:51:59 -0800

    Five

--- shownvidia.sh
+++ shownvidia.sh
@@ -15,10 +15,10 @@ for ((GPU=0;GPU<=NUMGPU;GPU++)); do
   SPEED=$("$SMICMD" --query-gpu=fan.speed --format=csv -i $GPU | grep -e [0-9+])
   SPEED=${SPEED:0:-1}
 
-  MEM=$(/mnt/c/Program\ Files/NVIDIA\ Corporation/NVSMI/nvidia-smi.exe --query-gpu=memory.used --format=csv -i $GPU | grep -e [0-9+])
+  MEM=$("$SMICMD" --query-gpu=memory.used --format=csv -i $GPU | grep -e [0-9+])
   MEM=${MEM:0:-1}
 
-  TEMP=$(/mnt/c/Program\ Files/NVIDIA\ Corporation/NVSMI/nvidia-smi.exe --query-gpu=temperature.gpu --format=csv -i $GPU | grep -e [0-9+])
+  TEMP=$("$SMICMD" --query-gpu=temperature.gpu --format=csv -i $GPU | grep -e [0-9+])
   TEMP=${TEMP:0:-1}
 
   echo "<tr><td>$GPU</td><td>$GPUCARD</td><td>$SPEED</td><td>$MEM</td><td>$TEMP c</td></tr>" | ssh metrics@10.0.0.2 "cat >> $COMPUTER.metrics"
