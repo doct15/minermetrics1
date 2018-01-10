d7b99618725b9a14e8561e5235b02ec29eb510ef
Brian McGehee <doc@tavian.com>
2018-01-09 21:10:20 -0800

    Seven

--- shownvidia.sh
+++ shownvidia.sh
@@ -5,25 +5,31 @@
 # COMPUTER="gamer"      - Name of miner
 # GPUCARD="GTX 1080 ti" - Type of GPU Card
 
-DATE=$(date '+%Y/%m/%d %H:%M:%S')
+while true; do
 
-echo "<tr><th COLSPAN=5>$COMPUTER $DATE</th></tr>" | ssh metrics@10.0.0.2 "cat > $COMPUTER.metrics"
-echo "<tr><th>GPU</th><th>CARD</th><th>SPEED</th><th>MEM</th><th>TEMP</th></tr>" | ssh metrics@10.0.0.2 "cat >> $COMPUTER.metrics"
+  DATE=$(date '+%Y/%m/%d %H:%M:%S')
 
-for ((GPU=0;GPU<=NUMGPU;GPU++)); do
+  echo "<tr><th COLSPAN=5>$COMPUTER $DATE</th></tr>" | ssh metrics@10.0.0.2 "cat > $COMPUTER.metrics"
+  echo "<tr><th>GPU</th><th>CARD</th><th>SPEED</th><th>MEM</th><th>TEMP</th></tr>" | ssh metrics@10.0.0.2 "cat >> $COMPUTER.metrics"
 
-  SPEED=$("$SMICMD" --query-gpu=fan.speed --format=csv -i $GPU | grep -e [0-9+])
-  MEM=$("$SMICMD" --query-gpu=memory.used --format=csv -i $GPU | grep -e [0-9+])
-  TEMP=$("$SMICMD" --query-gpu=temperature.gpu --format=csv -i $GPU | grep -e [0-9+])
-  if [ $COMPUTER != "linux" ]; then
-    SPEED=${SPEED:0:-1}
-    MEM=${MEM:0:-1}
-    TEMP=${TEMP:0:-1}
-  fi
+  for ((GPU=0;GPU<=NUMGPU;GPU++)); do
 
-  echo "<tr><td>$GPU</td><td>$GPUCARD</td><td>$SPEED</td><td>$MEM</td><td>$TEMP c</td></tr>" | ssh metrics@10.0.0.2 "cat >> $COMPUTER.metrics"
+    SPEED=$("$SMICMD" --query-gpu=fan.speed --format=csv -i $GPU | grep -e [0-9+])
+    MEM=$("$SMICMD" --query-gpu=memory.used --format=csv -i $GPU | grep -e [0-9+])
+    TEMP=$("$SMICMD" --query-gpu=temperature.gpu --format=csv -i $GPU | grep -e [0-9+])
+    if [ $COMPUTER != "linux" ]; then
+      SPEED=${SPEED:0:-1}
+      MEM=${MEM:0:-1}
+      TEMP=${TEMP:0:-1}
+    fi
+
+    echo "<tr><td>$GPU</td><td>$GPUCARD</td><td>$SPEED</td><td>$MEM</td><td>$TEMP c</td></tr>" | ssh metrics@10.0.0.2 "cat >> $COMPUTER.metrics"
+
+  done
+
+  # echo "</table>" | ssh metrics@10.0.0.2 "cat >> $COMPUTER.metrics"
+  echo "<!--DONE-->" | ssh metrics@10.0.0.2 "cat >> $COMPUTER.metrics"
+
+  sleep 20
 
 done
-
-# echo "</table>" | ssh metrics@10.0.0.2 "cat >> $COMPUTER.metrics"
-echo "DONE" | ssh metrics@10.0.0.2 "cat >> $COMPUTER.metrics"
