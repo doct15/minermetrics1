27857f430617944463e1a124bf9d9edd079a7cb5
Brian McGehee <doc@tavian.com>
2018-01-09 21:37:18 -0800

    asdf

--- assemble.sh
+++ assemble.sh
@@ -2,16 +2,20 @@
 #
 export DIR_TO_FILES="/home/metrics"
 
-echo "<table border=1>" > $DIR_TO_FILES/webfile.html
+while true; do
 
-for FILENAME in "$(ls $DIR_TO_FILES/*.metrics)"; do
-  echo "$FILENAME"
-  FILEDATA=""
-  while [ "${FILEDATA: -11}" != "<!--DONE-->" ]; do
-    FILEDATA=$(cat $FILENAME)
-    echo "$FILEDATA"
+  echo "<table border=1>" > $DIR_TO_FILES/webfile.html
+
+  for FILENAME in "$(ls $DIR_TO_FILES/*.metrics)"; do
+    echo "$FILENAME"
+    FILEDATA=""
+    while [ "${FILEDATA: -11}" != "<!--DONE-->" ]; do
+      FILEDATA=$(cat $FILENAME)
+      echo "$FILEDATA"
+    done
+    echo "$FILEDATA" >> $DIR_TO_FILES/webfile.html
   done
-  echo "$FILEDATA" >> $DIR_TO_FILES/webfile.html
-done
 
-echo "</table>" >> $DIR_TO_FILES/webfile.html
+  echo "</table>" >> $DIR_TO_FILES/webfile.html
+
+done
