0a01502a9251f99ad01ada57ca625d96757adbe1
Brian McGehee <doc@tavian.com>
2018-01-09 21:26:15 -0800

    asdf

--- assemble.sh
+++ assemble.sh
@@ -6,6 +6,9 @@ echo "<table border=1>" > $DIR_TO_FILES/webfile.html
 
 for FILENAME in "$(ls $DIR_TO_FILES/*.metrics)"; do
   echo "$FILENAME"
-  FILEDATA=$(cat $FILENAME)
-  echo "$FILEDATA"
+  FILEDATA=""
+  while ${FILEDATA: -11} != "<!--DONE-->"; do
+    FILEDATA=$(cat $FILENAME)
+    echo "$FILEDATA"
+  done
 done
