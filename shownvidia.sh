d5e588b08ffd7f37bd8ac21ae36977abcac5b0c1
Brian McGehee <doc@tavian.com>
2018-01-09 20:57:11 -0800

    Six

--- shownvidia.sh
+++ shownvidia.sh
@@ -13,13 +13,13 @@ echo "<tr><th>GPU</th><th>CARD</th><th>SPEED</th><th>MEM</th><th>TEMP</th></tr>"
 for ((GPU=0;GPU<=NUMGPU;GPU++)); do
 
   SPEED=$("$SMICMD" --query-gpu=fan.speed --format=csv -i $GPU | grep -e [0-9+])
-  SPEED=${SPEED:0:-1}
-
   MEM=$("$SMICMD" --query-gpu=memory.used --format=csv -i $GPU | grep -e [0-9+])
-  MEM=${MEM:0:-1}
-
   TEMP=$("$SMICMD" --query-gpu=temperature.gpu --format=csv -i $GPU | grep -e [0-9+])
-  TEMP=${TEMP:0:-1}
+  if [ $COMPUTER != "linux" ]; then
+    SPEED=${SPEED:0:-1}
+    MEM=${MEM:0:-1}
+    TEMP=${TEMP:0:-1}
+  fi
 
   echo "<tr><td>$GPU</td><td>$GPUCARD</td><td>$SPEED</td><td>$MEM</td><td>$TEMP c</td></tr>" | ssh metrics@10.0.0.2 "cat >> $COMPUTER.metrics"
 
