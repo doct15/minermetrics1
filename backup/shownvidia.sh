bf8d807b28157caf59f519ca34c9d16c855aa1b5
Brian McGehee <doc@tavian.com>
2018-01-13 17:05:05 -0800

    subdirs

--- backup/shownvidia.sh
+++ backup/shownvidia.sh
@@ -4,18 +4,19 @@
 # NUMGPU=0              - Number of GPUs
 # COMPUTER="gamer"      - Name of miner
 # GPUCARD="GTX 1080 ti" - Type of GPU Card
-DIR_TO_FILES="minermetrics1\worker_files"
-WORKING_FILES="temp"
+
 WAIT=25
 
 while true; do
 
   DATE=$(date '+%Y/%m/%d %H:%M:%S')
+  echo "Starting - $COMPUTER - $DATE"
 
-  echo "<tr><th COLSPAN=5>$COMPUTER $DATE</th></tr>" > "$WORKING_FILES/$COMPUTER.metrics"
-  echo "<tr><th>GPU</th><th>CARD</th><th>SPEED</th><th>MEM</th><th>TEMP</th></tr>" >> "$WORKING_FILES/$COMPUTER.metrics"
+  echo "<tr><th COLSPAN=5>$COMPUTER $DATE</th></tr>" | ssh metrics@10.0.0.2 "cat > $COMPUTER.metrics"
+  echo "<tr><th>GPU</th><th>CARD</th><th>SPEED</th><th>MEM</th><th>TEMP</th></tr>" | ssh metrics@10.0.0.2 "cat >> $COMPUTER.metrics"
 
   for ((GPU=0;GPU<=NUMGPU;GPU++)); do
+
     SPEED=$("$SMICMD" --query-gpu=fan.speed --format=csv -i $GPU | grep -e [0-9+])
     MEM=$("$SMICMD" --query-gpu=memory.used --format=csv -i $GPU | grep -e [0-9+])
     TEMP=$("$SMICMD" --query-gpu=temperature.gpu --format=csv -i $GPU | grep -e [0-9+])
@@ -24,11 +25,16 @@ while true; do
       MEM=${MEM:0:-1}
       TEMP=${TEMP:0:-1}
     fi
-    echo "<tr><td>$GPU</td><td>$GPUCARD</td><td>$SPEED</td><td>$MEM</td><td>$TEMP c</td></tr>" >> "$WORKING_FILES/$COMPUTER.metrics"
+
+    echo "<tr><td>$GPU</td><td>$GPUCARD</td><td>$SPEED</td><td>$MEM</td><td>$TEMP c</td></tr>" | ssh metrics@10.0.0.2 "cat >> $COMPUTER.metrics"
+    echo "GPU:$GPU - CARD:$GPUCARD - FAN:$SPEED - MEM:$MEM - TEMP:$TEMP c"
+
   done
 
-  echo "<!--DONE-->" | ssh metrics@10.0.0.2 "cat >> $WORKING_FILES/$DIR_TO_FILES/$COMPUTER.metrics"
-  cat $WORKING_FILES/$DIR_TO_FILES/$COMPUTER.metrics | ssh metrics@10.0.0.2 "cat >> $DIR_TO_FILES/$COMPUTER.metrics"
+  # echo "</table>" | ssh metrics@10.0.0.2 "cat >> $COMPUTER.metrics"
+  echo "<!--DONE-->" | ssh metrics@10.0.0.2 "cat >> $COMPUTER.metrics"
+
+  echo "Waiting..."
 
   sleep $WAIT
 
