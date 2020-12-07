529cfae288a3f220e942623aca9cb19dc60b1e17
Brian McGehee <doct15@yahoo.com>
2020-12-06 18:19:16 -0800

    add back in test files

---
+++ backup/shownvidia.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Set the following env variables before running this script
+# NUMGPU=0              - Number of GPUs
+# COMPUTER="gamer"      - Name of miner
+# GPUCARD="GTX 1080 ti" - Type of GPU Card
+
+WAIT=25
+
+while true; do
+
+  DATE=$(date '+%Y/%m/%d %H:%M:%S')
+  echo "Starting - $COMPUTER - $DATE"
+
+  echo "<tr><th COLSPAN=5>$COMPUTER $DATE</th></tr>" | ssh metrics@10.0.0.2 "cat > $COMPUTER.metrics"
+  echo "<tr><th>GPU</th><th>CARD</th><th>SPEED</th><th>MEM</th><th>TEMP</th></tr>" | ssh metrics@10.0.0.2 "cat >> $COMPUTER.metrics"
+
+  for ((GPU=0;GPU<=NUMGPU;GPU++)); do
+
+    SPEED=$("$SMICMD" --query-gpu=fan.speed --format=csv -i $GPU | grep -e [0-9+])
+    MEM=$("$SMICMD" --query-gpu=memory.used --format=csv -i $GPU | grep -e [0-9+])
+    TEMP=$("$SMICMD" --query-gpu=temperature.gpu --format=csv -i $GPU | grep -e [0-9+])
+    if [ $COMPUTER != "linux" ]; then
+      SPEED=${SPEED:0:-1}
+      MEM=${MEM:0:-1}
+      TEMP=${TEMP:0:-1}
+    fi
+
+    echo "<tr><td>$GPU</td><td>$GPUCARD</td><td>$SPEED</td><td>$MEM</td><td>$TEMP c</td></tr>" | ssh metrics@10.0.0.2 "cat >> $COMPUTER.metrics"
+    echo "GPU:$GPU - CARD:$GPUCARD - FAN:$SPEED - MEM:$MEM - TEMP:$TEMP c"
+
+  done
+
+  # echo "</table>" | ssh metrics@10.0.0.2 "cat >> $COMPUTER.metrics"
+  echo "<!--DONE-->" | ssh metrics@10.0.0.2 "cat >> $COMPUTER.metrics"
+
+  echo "Waiting..."
+
+  sleep $WAIT
+
+done
